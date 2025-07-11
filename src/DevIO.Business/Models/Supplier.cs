5d53ac27bdaa55bfc1817a8c8a1779e666cbbd63
sandro.vieira <sandro.vieira@ambevtech.com.br>
2025-07-11 16:01:52 -0300

    feat: Business tier: creating the entitites

---
+++ src/DevIO.Business/Models/Supplier.cs
@@ -0,0 +1,10 @@
+﻿namespace DevIO.Business.Models;
+
+public class Supplier : Entity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Document { get; set; } = string.Empty;
+    public SupplierType SupplierType { get; set; } = SupplierType.Undefined;
+    public Address address { get; set; } = new();
+    public bool IsActive { get; set; }
+}
---
+++ src/DevIO.Business/Models/SupplierType.cs
@@ -0,0 +1,8 @@
+﻿namespace DevIO.Business.Models;
+
+public enum SupplierType
+{
+    Undefined = 0,
+    Individual,
+    LegalEntity
+}
