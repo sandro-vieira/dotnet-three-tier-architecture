0c2c309cd18c7dc4f882cdab368d30b4fb79c78e
sandro.vieira <sandro.vieira@ambevtech.com.br>
2025-07-25 17:25:34 -0300

    feat: new services interfaces

---
+++ src/DevIO.Business/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces;
+
+public interface IProductService : IDisposable
+{
+    Task Add(Product product);
+    Task Update(Product product);
+    Task Delete(Guid id);
+}
---
+++ src/DevIO.Business/Interfaces/ISupplierService.cs
@@ -0,0 +1,10 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces;
+
+public interface ISupplierService : IDisposable
+{
+    Task Add(Supplier supplier);
+    Task Update(Supplier supplier);
+    Task Delete(Guid id);
+}
