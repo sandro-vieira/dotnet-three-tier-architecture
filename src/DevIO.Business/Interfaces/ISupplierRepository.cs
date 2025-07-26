3a441bc36247014af35a9e07894b80fa74d222fb
Sandro Vieira <=>
2025-07-26 13:11:01 -0300

    feat: Developing the Business Layer: Defining Persistence Interfaces

---
+++ src/DevIO.Business/Interfaces/ISupplierRepository.cs
@@ -0,0 +1,10 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces;
+
+public interface ISupplierRepository : IRepository<Supplier>
+{
+    Task<Supplier> GetSupplierAddressAsync(Guid id);
+    Task<Supplier> GetSupplierProductsAddressAsync(Guid id);
+    Task<Address> GetAddressBySupplierAsync(Guid supplierId);
+}
