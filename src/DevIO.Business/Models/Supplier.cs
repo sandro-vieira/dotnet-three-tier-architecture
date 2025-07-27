77c7f85f7265c7a431988ac5b7bd84786edaca3f
Sandro Vieira <=>
2025-07-27 13:46:23 -0300

    feat: Developing the Business Layer: Implementing Validations

--- src/DevIO.Business/Models/Supplier.cs
+++ src/DevIO.Business/Models/Supplier.cs
@@ -5,6 +5,6 @@ public class Supplier : Entity
     public string Name { get; set; } = string.Empty;
     public string Document { get; set; } = string.Empty;
     public SupplierType SupplierType { get; set; } = SupplierType.Undefined;
-    public Address address { get; set; } = new();
+    public Address Address { get; set; } = new();
     public bool IsActive { get; set; }
 }
