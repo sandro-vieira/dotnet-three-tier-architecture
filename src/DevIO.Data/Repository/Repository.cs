cd3b6004ed7145ea2d7059c117848b2917d48117
Sandro Vieira <=>
2025-08-22 12:45:12 -0300

    feat: Developing the Data Access Layer: Defining a Generic Repository

---
+++ src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq.Expressions;
+using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Repository;
+
+public abstract class Repository<TEntity> : IRepository<TEntity>
+    where TEntity : Entity, new()
+{
+    protected readonly CustomDbContext _db;
+    protected readonly DbSet<TEntity> _dbSet;
+    private bool _disposed;
+
+    protected Repository(CustomDbContext db)
+    {
+        _db = db;
+        _dbSet = db.Set<TEntity>();
+    }
+
+    public virtual async Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        => await _dbSet.FindAsync([id], cancellationToken);
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken)
+        => await _dbSet.ToListAsync(cancellationToken);
+
+    public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken)
+        => await _dbSet.AsNoTracking().Where(predicate).ToListAsync(cancellationToken);
+
+    public async Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken)
+        => await _dbSet.AnyAsync(e => e.Id == id, cancellationToken);
+
+    public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await _dbSet.AddAsync(entity, cancellationToken);
+        await SaveChangesAsync(cancellationToken);
+    }
+
+    public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        _dbSet.Update(entity);
+        await SaveChangesAsync(cancellationToken);
+    }
+
+    public virtual async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        _dbSet.Remove(new TEntity { Id = id });
+        await SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        => await _db.SaveChangesAsync(cancellationToken);
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+
+            _disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
