f4a9120936b398ebb39134c5e23f8cc0b8205924
Sandro Vieira <=>
2025-09-02 14:56:20 -0300

    feat: Connecting the Application to the Architecture: Mapping the ViewModels

---
+++ src/DevIO.Api/ViewModels/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.ViewModels;
+
+public class ProductViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
+    public Guid SupplierId { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(200, ErrorMessage = "The field {0} needs to have between {2} and {1} characters", MinimumLength = 2)]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(1000, ErrorMessage = "The field {0} needs to have between {2} and {1} characters", MinimumLength = 2)]
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
+    public decimal Price { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public bool Active { get; set; }
+
+    public string SupplierName { get; set; }
+}
---
+++ src/DevIO.Api/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.ViewModels;
+
+public class SupplierViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(100, ErrorMessage = "The field {0} needs to have between {2} and {1} characters", MinimumLength = 2)]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "The field {0} is required")]
+    [StringLength(14, ErrorMessage = "The field {0} needs to have between {2} and {1} characters", MinimumLength = 11)]
+    public string Document { get; set; }
+    
+    public int SupplierType { get; set; }
+    
+    public bool Active { get; set; }
+    
+    public AddressViewModel Address { get; set; }
+    public IEnumerable<ProductViewModel> Products { get; set; }
+}
