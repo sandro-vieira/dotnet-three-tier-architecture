3d9cc6200bbac21e969c4226f2d22a46b036f8df
Sandro Vieira <=>
2025-09-03 11:51:27 -0300

    feat: Connecting the Application to the Architecture: Business features

--- src/DevIO.Api/Controllers/CustomController.cs
+++ src/DevIO.Api/Controllers/CustomController.cs
@@ -1,8 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace DevIO.Api.Controllers;
 
 [ApiController]
 public abstract class CustomController : ControllerBase
 {
+    protected bool IsValidOperation() => true;
+
+    protected ActionResult CustomResponse(object? result = null)
+    {
+        if (IsValidOperation())
+        {
+            return Ok(result);
+        }
+        return BadRequest(new
+        {
+            // Get Errors
+        });
+    }
+
+    protected ActionResult CustomResponse(ModelStateDictionary modelState)
+    {
+        if (!modelState.IsValid)
+        {
+            // Notify Errors 
+        }
+
+        return CustomResponse();
+    }
+
+    protected void NotifyError(string message)
+    {
+        // Notify Error
+    }
 }
--- src/DevIO.Api/Controllers/ProductsController.cs
+++ src/DevIO.Api/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using DevIO.Api.ViewModels;
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.Api.Controllers;
@@ -6,37 +9,102 @@ namespace DevIO.Api.Controllers;
 [Route("api/products")]
 public class ProductsController : CustomController
 {
-    public ProductsController()
+    private readonly IProductRepository _productRepository;
+    private readonly IProductService _productService;
+    private readonly IMapper _mapper;
+
+    public ProductsController(
+        IProductRepository productRepository,
+        IProductService productService,
+        IMapper mapper)
     {
-      
+        _productRepository = productRepository;
+        _productService = productService;
+        _mapper = mapper;
     }
 
     [HttpGet]
     public async Task<IEnumerable<ProductViewModel>> GetAllAsync(CancellationToken cancellationToken)
     {
-
+        var collection = await _productRepository.GetProductsSuppliersAsync(cancellationToken);
+        return _mapper.Map<IEnumerable<ProductViewModel>>(collection);
     }
 
     [HttpGet("{id:guid}")]
     public async Task<ActionResult<ProductViewModel>> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
+        var productViewModel = await GetProductAsync(id, cancellationToken);
 
+        if (productViewModel == null)
+        {
+            return NotFound();
+        }
+
+        return productViewModel;
     }
 
+    [HttpPost]
     public async Task<ActionResult<ProductViewModel>> AddAsync(ProductViewModel productViewModel, CancellationToken cancellationToken)
     {
+        if (!ModelState.IsValid)
+        {
+            return CustomResponse(ModelState);
+        }
 
+        await _productService.AddAsync(_mapper.Map<Product>(productViewModel), cancellationToken);
+
+        return CustomResponse(productViewModel);
     }
 
     [HttpPut("{id:guid}")]
     public async Task<IActionResult> UpdateAsync(Guid id, ProductViewModel productViewModel, CancellationToken cancellationToken)
     {
+        if (id != productViewModel.Id)
+        {
+            NotifyError("The provided id does not match the product id.");
+            return CustomResponse();
+        }
 
+        if (!ModelState.IsValid)
+        {
+            return CustomResponse(ModelState);
+        }
+
+        var existingProduct = await GetProductAsync(id, cancellationToken);
+
+        if (existingProduct == null)
+        {
+            return NotFound();
+        }
+
+        existingProduct.SupplierId = productViewModel.SupplierId;
+        existingProduct.Name = productViewModel.Name;
+        existingProduct.Description = productViewModel.Description;
+        existingProduct.Price = productViewModel.Price;
+        existingProduct.IsActive = productViewModel.IsActive;
+
+        await _productService.UpdateAsync(_mapper.Map<Product>(existingProduct), cancellationToken);
+
+        return CustomResponse();
     }
 
     [HttpDelete("{id:guid}")]
     public async Task<ActionResult<ProductViewModel>> DeleteAsync(Guid id, CancellationToken cancellationToken)
     {
+        var existingProduct = await GetProductAsync(id, cancellationToken);
+        if (existingProduct == null)
+        {
+            return NotFound();
+        }
 
+        await _productService.DeleteAsync(id, cancellationToken);
+
+        return CustomResponse();
+    }
+
+    private async Task<ProductViewModel> GetProductAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetProductSupplierAsync(id, cancellationToken);
+        return _mapper.Map<ProductViewModel>(product);
     }
 }
